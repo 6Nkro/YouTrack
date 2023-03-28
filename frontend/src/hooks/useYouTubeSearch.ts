27fae61cb868197a8a2a36a0cb1dab874751768a
hub-coco <kij1467@gmail.com>
2023-03-28 14:32:41 +0900

    separate branch

--- frontend/src/hooks/useYouTubeSearch.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "react-query";
-import { getSearchResult } from "../services/YouTubeSearchService";
-
-export function useYouTubeSearch() {
-  return useQuery("searchResult", () => getSearchResult(""), {
-    enabled: false,
-    retry: false,
-  });
-}
